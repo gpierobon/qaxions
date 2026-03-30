ce656e47808b579eda3b5a70b57d8b56529183f6
gpierobon <gpierobon@users.noreply.github.com>
2026-03-30 12:18:21 +1100

    update collect script

--- scripts/collect-times.sh
+++ scripts/collect-times.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-output="gadi_256.txt"
+INPUT=$1
+TYPE=$2
 
-for dir in mac_o_128_*thr/; do
+output="gadi_256_$TYPE.txt"
+if [ -f $output ]; then
+    rm $output
+fi
+
+echo "$output"
+
+for dir in ${INPUT}_*thr/; do
+#for dir in o_256_*thr/; do
     # Strip trailing slash, then extract the number before 'thr'
     dirname="${dir%/}"
     nthr=$(echo "$dirname" | sed 's/.*_\([0-9]*\)thr$/\1/')
 
     logfile="$dir/profile.log"
     if [[ -f "$logfile" ]]; then
-        runtime=$(grep "Total" "$logfile" | awk '{print $3}')
+        case $2 in 
+            total)
+                runtime=$(grep "Total" "$logfile" | awk '{print $3}')
+            ;;
+            kick)
+                runtime=$(grep "KICK" "$logfile" | awk '{print $2}')
+            ;;
+            drift) 
+                runtime=$(grep "DRIFT" "$logfile" | awk '{print $2}')
+            ;;
+            poisson) 
+                runtime=$(grep "POISSON" "$logfile" | awk '{print $2}')
+            ;;
+            fft) 
+                runtime=$(grep "FFT" "$logfile" | awk '{print $2}')
+            ;;
+        esac
         echo "$nthr $runtime" >> "$output"
     else
         echo "WARNING: $logfile not found" >&2
